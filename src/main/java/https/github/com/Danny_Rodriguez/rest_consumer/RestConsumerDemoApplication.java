1dbefd80ef7e09700c0cdab3091866d11094dbb3
Danny-Rodriguez <3039433+Danny-Rodriguez@users.noreply.github.com>
2025-02-21 12:42:29 -0800

    first commit

---
+++ src/main/java/https/github/com/Danny_Rodriguez/rest_consumer/RestConsumerDemoApplication.java
@@ -0,0 +1,13 @@
+package https.github.com.Danny_Rodriguez.rest_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestConsumerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestConsumerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/https/github/com/Danny_Rodriguez/rest_consumer/RestConsumerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package https.github.com.Danny_Rodriguez.rest_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestConsumerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
